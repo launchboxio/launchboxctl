7fe7fbef499eed04b180c0c737774e61817926e6
Robert Wittman <robkwittman@gmail.com>
2023-05-02 22:05:12 -0400

    Initialize Cobra

---
+++ cmd/root.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "lbx",
+	Short: "Command Line for LaunchboxHQ",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Launchbox is initialized!")
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
